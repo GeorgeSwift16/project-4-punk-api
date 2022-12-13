e40aaee306702e4534f2bf77161e7bc2526bd1c9
GeorgeSwift16 <George.Swift@nology.io>
2022-12-13 18:28:50 +0000

    search functionality refactored and complete

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -5,24 +5,22 @@ import { useEffect, useState } from "react";
 
 const Main = () => {
   const getSearchInput = (event) => {
-    const userSearchInput = event.target.value;
+    let initialUserSearchInput = event.target.value;
+    // to refactor back to usersearchinput = event.target.value if no way to make search strictly as typed.
+    const userSearchInput = initialUserSearchInput.replace(" ", "_");
+    console.log(userSearchInput);
     setsearchInput(userSearchInput);
   };
 
-  const getFilterInput = (event) => {};
+  // const getFilterInput = (event) => {};
 
   const [beers, setBeers] = useState([]);
   const [searchInput, setsearchInput] = useState("");
 
   const getBeers = async (search) => {
     let url = `https://api.punkapi.com/v2/beers`;
-
-    if (!searchInput) {
-      url = `https://api.punkapi.com/v2/beers?page=1&per_page=80`;
-    }
-    if (searchInput) {
-      url = `https://api.punkapi.com/v2/beers?beer_name=${search}`;
-    }
+    // if search query exists add search query, if not show default.
+    search ? (url += `?beer_name=${search}`) : (url += `?page=1&per_page=80`);
     const response = await fetch(url);
     const data = await response.json();
     setBeers(data);
