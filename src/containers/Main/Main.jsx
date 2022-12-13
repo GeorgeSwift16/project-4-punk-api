8ebf4b95a529832fc27a76503a65266bb231a9e5
GeorgeSwift16 <George.Swift@nology.io>
2022-12-13 18:11:29 +0000

    search functionality working, commit prior to refactoring

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,15 +1,40 @@
 import "./Main.scss";
 import CardBlock from "../CardBlock/CardBlock";
 import NavigationMenu from "../NavigationMenu/NavigationMenu";
+import { useEffect, useState } from "react";
 
-const Main = ({ beersArray }) => {
+const Main = () => {
   const getSearchInput = (event) => {
-    console.log(event.target.value);
+    const userSearchInput = event.target.value;
+    setsearchInput(userSearchInput);
   };
+
+  const getFilterInput = (event) => {};
+
+  const [beers, setBeers] = useState([]);
+  const [searchInput, setsearchInput] = useState("");
+
+  const getBeers = async (search) => {
+    let url = `https://api.punkapi.com/v2/beers`;
+
+    if (!searchInput) {
+      url = `https://api.punkapi.com/v2/beers?page=1&per_page=80`;
+    }
+    if (searchInput) {
+      url = `https://api.punkapi.com/v2/beers?beer_name=${search}`;
+    }
+    const response = await fetch(url);
+    const data = await response.json();
+    setBeers(data);
+  };
+  useEffect(() => {
+    getBeers(searchInput);
+  }, [searchInput]);
+
   return (
     <main className="main-content">
       <NavigationMenu searchInput={getSearchInput} />
-      <CardBlock beersArray={beersArray} />
+      <CardBlock beersArray={beers} />
     </main>
   );
 };
